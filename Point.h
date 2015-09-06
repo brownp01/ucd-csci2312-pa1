5de2a3673ba074af98adeb57369980f241dff71b
Paul Brown <Paul@Paul-Browns-iMac.local>
2015-09-06 16:52:37 -0600

    First commit of PA1

--- Point.h
+++ Point.h
@@ -1,4 +1,4 @@
-// A 2-dimensional point class!
+// A 3-dimensional point class!
 // Coordinates are double-precision floating point.
 
 #ifndef __point_h
@@ -9,11 +9,13 @@ class Point {
 private:
   double x;
   double y;
+  double z;
 
 public:
   // Constructors
   Point();                      // default constructor
-  Point(double x, double y);    // two-argument constructor
+  Point(double x, double y, double z);    // three-argument constructor
+  double distanceTo(Point &); //one argument member function using Point class
 
   // Destructor
   ~Point();
@@ -21,10 +23,12 @@ public:
   // Mutator methods
   void setX(double newX);
   void setY(double newY);
+  void setZ(double newZ);
 
   // Accessor methods
   double getX();
   double getY();
+  double getZ();
 };
 
 #endif // __point_h
--- pa1.cpp
+++ pa1.cpp
@@ -1,11 +1,94 @@
 //
-// Created by Ivo Georgiev on 8/25/15.
+// Created by Paul Brown on 09/06/15.
 //
 
 #include "Point.h"
+#include <iostream>
+#include <cmath>
+#include <iomanip>
 
-int main(void) {
-    Point p;
+using namespace std;
+
+void computeArea(double, double, double);
+
+int main() {
+
+    Point a;
+    Point b;
+    Point c;
+
+    double x = 0;
+    double y = 0;
+    double z = 0;
+    double distA = 0;
+    double distB = 0;
+    double distC = 0;
+
+    cout << "Please enter coordinates for Point A" << endl << endl;
+    cout << "x = ";
+    cin >> x;
+    cout << "y = ";
+    cin >> y;
+    cout << "z = ";
+    cin >> z;
+    cout << endl;
+    a.setX(x);
+    a.setY(y);
+    a.setZ(z);
+
+    cout << "Please enter coordinates for Point B" << endl << endl;
+    cout << "x = ";
+    cin >> x;
+    cout << "y = ";
+    cin >> y;
+    cout << "z = ";
+    cin >> z;
+    cout << endl;
+    b.setX(x);
+    b.setY(y);
+    b.setZ(z);
+
+    cout << "Please enter coordinates for Point C" << endl << endl;
+    cout << "x = ";
+    cin >> x;
+    cout << "y = ";
+    cin >> y;
+    cout << "z = ";
+    cin >> z;
+    cout << endl;
+    c.setX(x);
+    c.setY(y);
+    c.setZ(z);
+
+    c.distanceTo(b);
+    distA = c.distanceTo(b);
+
+    a.distanceTo(c);
+    distB = a.distanceTo(c);
+
+    a.distanceTo(b);
+    distC = a.distanceTo(b);
+
+    cout << distA << endl;
+    cout << distB << endl;
+    cout << distC << endl;
+
+    computeArea(distA, distB, distC);
 
     return 0;
+}
+
+
+void computeArea(double d_A, double d_B, double d_C)
+{
+    double area = 0;
+    double side = 0;
+
+    side = (d_A + d_B + d_C)/2;
+    cout << " side = " << side << endl;
+
+    area = sqrt(side * (side - d_A) * (side - d_B) * (side - d_C));
+
+    cout << setprecision(2) << fixed;
+    cout << "The area of your triangle is " << area << endl;
 }
