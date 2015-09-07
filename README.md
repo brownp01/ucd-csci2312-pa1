aac5a394617413abb76fa824ac8682d78caf97a1
Paul Brown <paul.brown@ucdenver.edu>
2015-09-06 19:58:18 -0600

    First commit

--- README.md
+++ README.md
@@ -33,27 +33,13 @@ double getZ();
 
 1. User inputs x, y, z values for a given point.
 2. Inputs are inserted into the Point object.
-3. Distance is calculated between two points by utilizing the distanceTo member function. Length for each side is assigned. 
-4. computeArea function is called to determine the area of the 3D triangle by passing the values of length for a, b and c.
-5. Area is outputted to user. 
-6. Programs ends.
+3. computeArea function is called to determine the area of the 3D triangle by passing the values of the Point A, B and C objects.
+4. Inside computeArea, Distance is calculated between two points by utilizing the distanceTo member function. Length for each side is assigned. 
+5. Semiperimeter is calculated using the lengths of each side.
+6. Area is calculated and its value is returned to main.
+7. The area is outputted to user.
+8. Programs ends.
 
 
 
-
-7.  Go back and test <tt>computeArea</tt> with several boundary and/or extreme cases to make sure your implementation is robust for all triangles, including degenerate ones. Things you might try:
-
-*   Triangles with no area - all three points on the same line
-*   Triangles where two points are the same
-*   Triangles where all three points are the same
-*   Triangles with negative coordinate values
-*   etc.
-
-In software-engineering parlance, these degenerate cases are called "edge cases" or "corner cases," because they test combinations of inputs that may be rare, but still possible. Usually, you want to start out by seeing if your program can handle simple cases that will expose obvious issues. Once your program can handle these, then you move on to the more subtle test scenarios.
-
-8.  Update the README.md file in your local repository with a description of your 3D Point class. You can use this file as a guide.
-
-9.  Commit all code changes to your local Github repository, then push the changes to the master repository on Github.
-
-10. Submit the URL of your CSCI 2312 repository on Canvas.
 
--- pa1.cpp
+++ pa1.cpp
@@ -11,7 +11,7 @@ using namespace std;
 
 // Function prototype
 
-void computeArea(double, double, double);  //computes area of 3D triangle
+double computeArea(Point &, Point &, Point &);  //computes area of 3D triangle
 
 int main() {
 
@@ -22,9 +22,8 @@ int main() {
     double x = 0;           //user input for x value of a point
     double y = 0;           //user input for y value of a point
     double z = 0;           //user input for z value of a point
-    double lengthA = 0;     //value of length a
-    double lengthB = 0;     //value of length b
-    double lengthC = 0;     //value of length c
+    double area = 0;        //holds value for area
+
 
     cout << "Please enter coordinates for Point A" << endl << endl;
     cout << "x = ";
@@ -62,16 +61,13 @@ int main() {
     c.setY(y);      //inserts value of y into Point C of Point Class
     c.setZ(z);      //inserts value of z into Point C of Point Class
 
-    c.distanceTo(b);            //member function that calculates distance from Point C to Point B
-    lengthA = c.distanceTo(b);  //inserts return value of distance into length of a
 
-    a.distanceTo(c);            //member function that calculates distance from Point A to Point C
-    lengthB = a.distanceTo(c);  //inserts return value of distance into length of b
 
-    a.distanceTo(b);            //member function that calculates distance from Point A to Point B
-    lengthC = a.distanceTo(b);  //inserts return value of distance into length of c
+    area = computeArea(a, b, c); //call to function that will compute area from Point a, b, and c objects and return it
+                                 // to variable area.
 
-    computeArea(lengthA, lengthB, lengthC); //call to function that will compute area from length a, b, and c
+    cout << setprecision(2) << fixed;   //formatted output to two decimal places
+    cout << "The area of your triangle is " << area << endl;
 
     return 0;
 }
@@ -84,21 +80,32 @@ int main() {
 //        and then calculate the area of a 3D triangle. Area will be
 //        outputted to user.
 //
-//  data in: the lengths of a, b and c with double values.
+//  data in: Points a, b and c from Point Class.
 //  data out: none
 //
 //**************************************************************************
 
 
-void computeArea(double l_A, double l_B, double l_C)
+double computeArea(Point &a, Point &b, Point &c)
 {
-    double area = 0;    //holds value for area
-    double side = 0;    //holds value for semiperimeter
+    double s = 0;           //holds value for semiperimeter
+    double lengthA = 0;     //value of length a
+    double lengthB = 0;     //value of length b
+    double lengthC = 0;     //value of length c
 
-    side = (l_A + l_B + l_C)/2;     //formula to calculate semiperimeter of triangle
+    c.distanceTo(b);            //member function that calculates distance from Point C to Point B
+    lengthA = c.distanceTo(b);  //inserts return value of distance into length of a
 
-    area = sqrt(side * (side - l_A) * (side - l_B) * (side - l_C));     //formula to calculate area of triangle
+    a.distanceTo(c);            //member function that calculates distance from Point A to Point C
+    lengthB = a.distanceTo(c);  //inserts return value of distance into length of b
 
-    cout << setprecision(2) << fixed;   //formatted output to two decimal places
-    cout << "The area of your triangle is " << area << endl;
+    a.distanceTo(b);            //member function that calculates distance from Point A to Point B
+    lengthC = a.distanceTo(b);  //inserts return value of distance into length of c
+
+    s = (lengthA + lengthB + lengthC)/2;     //formula to calculate semiperimeter of triangle
+
+    return sqrt(s * (s - lengthA) * (s - lengthB) * (s - lengthC));     //formula to calculate area of triangle
+                                                                        //returns value to main
+
+
 }
