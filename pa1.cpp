b12a1be7bac0a78f150ac52a79b2725a378ace7f
Paul Brown <paul.brown@ucdenver.edu>
2015-09-07 23:36:05 -0600

    Second Commit

--- pa1.cpp
+++ pa1.cpp
@@ -93,14 +93,14 @@ double computeArea(Point &a, Point &b, Point &c)
     double lengthB = 0;     //value of length b
     double lengthC = 0;     //value of length c
 
-    c.distanceTo(b);            //member function that calculates distance from Point C to Point B
-    lengthA = c.distanceTo(b);  //inserts return value of distance into length of a
+    lengthA = c.distanceTo(b);  //member function that calculates distance from Point C to Point B
+                                //inserts return value of distance into length of a
 
-    a.distanceTo(c);            //member function that calculates distance from Point A to Point C
-    lengthB = a.distanceTo(c);  //inserts return value of distance into length of b
+    lengthB = a.distanceTo(c);  //member function that calculates distance from Point A to Point C
+                                //inserts return value of distance into length of b
 
-    a.distanceTo(b);            //member function that calculates distance from Point A to Point B
-    lengthC = a.distanceTo(b);  //inserts return value of distance into length of c
+    lengthC = a.distanceTo(b);  //member function that calculates distance from Point A to Point B
+                                //inserts return value of distance into length of c
 
     s = (lengthA + lengthB + lengthC)/2;     //formula to calculate semiperimeter of triangle
 
